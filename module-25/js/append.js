521db8869652bc64597cabc0de6d348f1c20bbed
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-16 13:21:29 +0600

    practice is done

---
+++ module-25/js/append.js
@@ -0,0 +1,7 @@
+// 1: where to add
+const placesList = document.getElementById('places-list');
+// 2: where to be add
+const li = document.createElement('li');
+li.innerText = 'pahartoli bon';
+// 3: add the child
+placesList.appendChild(li);
