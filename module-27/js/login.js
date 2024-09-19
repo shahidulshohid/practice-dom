1ce7d976a38a1f3b97125720ede35ba01573d6e9
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-19 10:11:05 +0600

    home page login practice starts

--- module-27/js/login.js
+++ module-27/js/login.js
@@ -6,9 +6,20 @@ document.getElementById('button-login').addEventListener('click', function(even)
     //step - 2 : prevent default behavior (prevent reloading browser)
     even.preventDefault();
 
-    //step - 3 : get the phone number
+    //step - 3 : get the phone number and the pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-    
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber)
+
+    //step - 4 : validate phone and pin
+    //this is temporary. you should not do like this
+    if(phoneNumber === '5' && pinNumber === '1234'){
+
+        // step - 5 : allow user to use the webpage 
+        window.location.href = './home.html'
+    }
+    else {
+        alert('Wrong phone number or pin number')
+    }
     
 })
