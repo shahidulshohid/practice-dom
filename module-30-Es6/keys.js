3796f2450875a624badb5b50b92230cba3418aad
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-26 11:00:26 +0600

    practise entries and keys and values of object

---
+++ module-30-Es6/keys.js
@@ -0,0 +1,10 @@
+const glass = {name:'glass', color:'golden', price:12, isCleaned: true}
+const keys = Object.keys(glass)
+const values = Object.values(glass)
+const entries = Object.entries(glass)
+
+console.log(keys);
+console.log(values)
+console.log(entries);
+
+console.log(glass)
